a3d4b3d58583c06d93eb6c5938dfa9588c25f8bf
STEngineer <pamnlyr@gmail.com>
2019-08-03 18:41:34 +0400

    Initial commit

---
+++ src/main/java/ru/ste/stevesseries/corebase/EventListener.java
@@ -0,0 +1,7 @@
+package ru.ste.stevesseries.corebase;
+
+import org.bukkit.event.Listener;
+
+public class EventListener implements Listener {
+
+}
---
+++ src/main/java/ru/ste/stevesseries/corebase/NMSUtil.java
@@ -0,0 +1,48 @@
+package ru.ste.stevesseries.corebase;
+
+import org.bukkit.Bukkit;
+import org.bukkit.entity.Player;
+
+import java.lang.reflect.InvocationTargetException;
+
+public class NMSUtil {
+    public static String getNMSVersion() {
+        return Bukkit.getServer().getClass().getPackage().getName().split("\\.")[3];
+    }
+    public static Class<?> getNMSClass(String name) {
+        try {
+            return Class.forName("net.minecraft.server." + getNMSVersion() + "." + name);
+        } catch (ClassNotFoundException e) {
+            return null;
+        }
+    }
+    public static Class<?> getOBCClass(String name) {
+        try {
+            return Class.forName("org.bukkit.craftbukkit." + getNMSVersion() + "." + name);
+        } catch (ClassNotFoundException e) {
+            return null;
+        }
+    }
+    public static Object getHandle(Player p) {
+        if(p == null) return null;
+        try {
+            return p.getClass().getMethod("getHandle").invoke(p);
+        } catch (IllegalAccessException | NoSuchMethodException | InvocationTargetException e) {
+            return null;
+        }
+    }
+    public static Object getConnection(Player p) {
+        Object handle = getHandle(p);
+        try {
+            return handle.getClass().getField("playerConnection").get(handle);
+        } catch (IllegalAccessException | NoSuchFieldException e) {
+            return null;
+        }
+    }
+    public static void sendPacket(Player p, Object packet) {
+        Object connection = getConnection(p);
+        try {
+            connection.getClass().getMethod("sendPacket", getNMSClass("Packet")).invoke(connection, packet);
+        } catch (IllegalAccessException | InvocationTargetException | NoSuchMethodException e) {}
+    }
+}
