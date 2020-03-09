e7a3389730afc69d0466cb483e596616d4f1bbfb
Steve <sthesengineer@gmail.com>
2020-03-09 18:38:52 +0400

    Improve ConfigValue, get rid of config value types

--- src/main/java/me/ste/stevesseries/corebase/CoreBase.java
+++ src/main/java/me/ste/stevesseries/corebase/CoreBase.java
@@ -1,22 +1,22 @@
 package me.ste.stevesseries.corebase;
 
-import me.ste.stevesseries.corebase.config.valuetype.ConfigValueType;
-import me.ste.stevesseries.corebase.config.valuetype.SimpleValueType;
-import me.ste.stevesseries.corebase.registry.Registry;
 import me.ste.stevesseries.corebase.registry.RegistryManager;
-import me.ste.stevesseries.corebase.registry.rule.ClassRule;
-import me.ste.stevesseries.corebase.registry.rule.NotNullRule;
-import org.bukkit.NamespacedKey;
 import org.bukkit.plugin.java.JavaPlugin;
 
+import javax.script.ScriptEngine;
+import javax.script.ScriptEngineManager;
+
 public final class CoreBase extends JavaPlugin {
+    public ScriptEngine scriptEngine = null;
+
     @Override
     public void onEnable() {
         RegistryManager.getInstance();
 
-        Registry configValueTypeRegistry = RegistryManager.getInstance().addRegistry(new NamespacedKey(this, "config_value_type"));
-        configValueTypeRegistry.addRules(new NotNullRule(), new ClassRule(ConfigValueType.class));
-        configValueTypeRegistry.register(new NamespacedKey(this, "simple"), new SimpleValueType());
+        scriptEngine = new ScriptEngineManager(null).getEngineByName("JavaScript");
+        if(scriptEngine == null) {
+            this.getLogger().warning("No JavaScript engine is available, JavaScript support is disabled");
+        }
 
         getServer().getPluginManager().registerEvents(new EventListener(), this);
     }
--- src/main/java/me/ste/stevesseries/corebase/config/ConfigValue.java
+++ src/main/java/me/ste/stevesseries/corebase/config/ConfigValue.java
@@ -1,41 +1,74 @@
 package me.ste.stevesseries.corebase.config;
 
-import me.ste.stevesseries.corebase.GenericUtil;
-import me.ste.stevesseries.corebase.config.valuetype.ConfigValueType;
-import me.ste.stevesseries.corebase.registry.Registry;
-import me.ste.stevesseries.corebase.registry.RegistryManager;
+import com.google.common.base.Preconditions;
+import me.ste.stevesseries.corebase.CoreBase;
 import org.bukkit.configuration.ConfigurationSection;
 
+import javax.script.Bindings;
+import javax.script.ScriptContext;
+import javax.script.ScriptException;
 import java.util.Map;
 
 public class ConfigValue {
-    private static Registry configValueTypeRegistry = RegistryManager.getInstance().getRegistryExceptional(GenericUtil.parseNamespacedKey("config_value_type"));
-    private ConfigValueType type;
     private ConfigurationSection section;
     private String path;
-    private Map<String, String> context;
 
-    public ConfigValue(ConfigurationSection section, String path, Map<String, String> context) {
+    public ConfigValue(ConfigurationSection section, String path) {
         this.section = section;
         this.path = path;
-        this.context = context;
-        if(section.contains("type")) {
-            this.type = (ConfigValueType) configValueTypeRegistry.getEntry(GenericUtil.parseNamespacedKey(section.get("type").toString())).get().getObject();
-        } else {
-            this.type = (ConfigValueType) configValueTypeRegistry.getEntry(GenericUtil.parseNamespacedKey("simple")).get().getObject();
+    }
+
+    public String getValue(Map<String, String> context) {
+        Preconditions.checkState(section.isString(path), "Config value must be a string");
+        Bindings bindings = CoreBase.getPlugin(CoreBase.class).scriptEngine.getBindings(ScriptContext.ENGINE_SCOPE);
+        bindings.clear();
+        bindings.put("section", section);
+        bindings.put("path", path);
+        bindings.put("env", context);
+        CoreBase.getPlugin(CoreBase.class).scriptEngine.setBindings(bindings, ScriptContext.ENGINE_SCOPE);
+        String str = section.getString(path);
+        StringBuilder result = new StringBuilder();
+        boolean dollarSign = false;
+        int bracketsOpen = 0;
+        int beginIndex = -1;
+        int lastEndIndex = 0;
+        char[] chars = str.toCharArray();
+        for(int i = 0; i < chars.length; i++) { // i is for index
+            char c = chars[i];
+            if(dollarSign) {
+                if(c == '{') {
+                    if(beginIndex == -1) {
+                        beginIndex = i + 1;
+                    }
+                    bracketsOpen++;
+                } else if(c == '}') {
+                    if(bracketsOpen > 0) {
+                        bracketsOpen--;
+                    }
+                    if(bracketsOpen <= 0) {
+                        String evalResult = "";
+                        try {
+                            evalResult = CoreBase.getPlugin(CoreBase.class).scriptEngine.eval(str.substring(beginIndex, i)).toString();
+                        } catch (ScriptException e) {
+                            e.printStackTrace();
+                        }
+                        result.append(str, lastEndIndex, beginIndex - 2);
+                        result.append(evalResult);
+                        lastEndIndex = i + 1;
+                        dollarSign = false;
+                        beginIndex = -1;
+                        bracketsOpen = 0;
+                    }
+                } else {
+                    dollarSign = false;
+                }
+            } else {
+                if(c == '$') {
+                    dollarSign = true;
+                }
+            }
         }
-    }
-
-    public String getValue() {
-        return type.getValue(this, section, path);
-    }
-
-    public Map<String, String> getContext() {
-        return context;
-    }
-
-    public String processStringCtx(String str) {
-        // TODO
-        return null;
+        result.append(str.substring(lastEndIndex));
+        return result.toString();
     }
 }
