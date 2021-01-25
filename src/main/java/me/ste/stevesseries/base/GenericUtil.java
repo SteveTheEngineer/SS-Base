a3c69621f9ccadec7425aeb67734dc0af9340f9b
SteveTheEngineer <sthesengineer@gmail.com>
2021-01-25 15:24:27 +0300

    Fixed parseNamespacedKey

--- src/main/java/me/ste/stevesseries/base/GenericUtil.java
+++ src/main/java/me/ste/stevesseries/base/GenericUtil.java
@@ -32,9 +32,9 @@ public final class GenericUtil {
      */
     public static NamespacedKey parseNamespacedKey(String defaultNamespace, String namespacedKey) {
         String[] split = namespacedKey.split(":");
-        if(namespacedKey.matches("[a-z0-9/._-]:[a-z0-9/._-]")) {
+        if(namespacedKey.matches("[a-z0-9/._-]+:[a-z0-9/._-]+")) {
             return new NamespacedKey(split[0], String.join(":", Arrays.copyOfRange(split, 1, split.length - 1)));
-        } else if(namespacedKey.matches("[a-z0-9/._-]")) {
+        } else if(namespacedKey.matches("[a-z0-9/._-]+")) {
             return new NamespacedKey(defaultNamespace, split[0]);
         } else {
             return null;
